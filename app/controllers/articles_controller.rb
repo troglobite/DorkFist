efc6c74e0cd063c34b90362a015f8ada97f59dc2
troglobite <brianparry089@gmail.com>
2018-10-09 20:02:00 -0600

    added the article create to the article controller, created the article show and index view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,26 @@ class ArticlesController < ApplicationController
     def new 
     end 
 
+    def index
+        @article = Article.all
+    end
+
     def create
-        
+        @article = Article.new(article_params)
+
+        @article.save
+        redirect_to @article
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    def new
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:title, :text)
     end
 end
