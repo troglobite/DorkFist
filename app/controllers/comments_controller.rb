30c64b7ae2948aef5a3caa5f1380bd37a9d8cc8a
troglobite <brianparry089@gmail.com>
2018-10-12 20:48:03 -0600

    Add update to the comments controller, creted a edit comment view, added link edit link to comment link in article view '

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,22 @@ class CommentsController < ApplicationController
         redirect_to article_path(@article)
     end
 
+    def edit
+        @article = Article.find(params[:id])
+        @comment = @article.comments.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @comment = @article.comments.find(params[:id])
+
+        if @comment.update(comment_params)
+            redirect_to @article
+        else 
+            render 'edit'
+        end
+    end
+
     private
         def comment_params
             params.require(:comment).permit(:commenter, :body)
