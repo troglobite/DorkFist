eed92b1559e77359b796fee4adbc88ebf5f1a022
troglobite <brianparry089@gmail.com>
2018-10-15 20:47:43 -0600

    Added current user check, and a require valid user to the application controller os it will check for a valid loggged in user on every page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    before_action :require_valid_user!
+
+    def current_user
+        if !session[:user_id].blank?
+            @user ||= User.find(session[:user_id])
+        end
+    end
+
+    def require_valid_user! 
+        if current_user.nil?
+            flash[:error] = "You must be logged in to access that page"
+            redirect_to login_path
+        end
+    end
 end
