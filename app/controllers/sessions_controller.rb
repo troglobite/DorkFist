d652044d58a8f6888ceaabbb1753db428d7e7475
troglobite <brianparry089@gmail.com>
2018-10-15 20:56:57 -0600

    added create and destroy to the session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create
+    reset_session
+    @user = User.find_by(email: [session_params[:email]])
+
+    if @user && @user.authenticate(session_params[:password])
+      session[:user_id] = @user.id
+      flash[:success] = 'Welcome back'  
+      redirect_to root_path
+    else
+      flash[:error] = 'Wrong email and password combination'
+      redirect_to login_path
+    end
   end
 
   def destroy
+    reset_session
+  end
+
+  def sessions_params
+    params.require(:session).permit(:email, :password)
   end
 end
