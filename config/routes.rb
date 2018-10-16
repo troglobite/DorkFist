5db5af3e6a5bb89a3acbf7e7a004c946578f1b9e
troglobite <brianparry089@gmail.com>
2018-10-15 20:39:56 -0600

    generated controllers for users, sessions and a dashboard -modified the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'users/new'
+  get 'login', to 'sessions#new'
+  get 'logout', to 'sessions#destroy'
+
+  resources :sessions, only: [:create]
+  resources :users, only: [:new, :create]
+
 
   resources :articles do
     resources :comments
