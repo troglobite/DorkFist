faf74684f53360682990aeec5fb5b2fd123dec7b
troglobite <brianparry089@gmail.com>
2018-10-16 19:15:59 -0600

    created the new, create methods in the user controller, added boot strap to the application view, added a log out link to the articles show view that will be serving as the welcome screen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
+  skip_before_action :require_valid_user!
+  before_action :reset_action
+
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save!
+      flash[:success] = "You have successfully created an account. Please sign in to continue"
+      redirect_to login_path
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
