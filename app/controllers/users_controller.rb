96cf7bfe70eade98eac4f91a0bf3d380ebae059c
troglobite <brianparry089@gmail.com>
2018-10-22 19:33:19 -0600

    created a show action on the user controller and added a new pathe, then a link to the a very basic user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show 
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
 
@@ -17,7 +21,8 @@ class UsersController < ApplicationController
     end
   end
 
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
-  end
+  private
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   get 'users/new'
   get 'login', to: 'sessions#new'
   get 'logout', to: 'sessions#destroy'
+  get 'users/:id', to: 'users#show'
 
   resources :sessions, only: [:create]
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
 
 
   resources :articles do
