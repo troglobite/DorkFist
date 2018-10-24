bbd4fb1f8ce2def3e91c34e6b6c7024c78b46445
troglobite <brianparry089@gmail.com>
2018-10-23 20:06:04 -0600

    fixed a migration i had generated wrong

--- db/migrate/20181024020334_add_user_id_to_likes.rb
+++ db/migrate/20181024020334_add_user_id_to_likes.rb
@@ -1,4 +1,5 @@
 class AddUserIdToLikes < ActiveRecord::Migration[5.2]
   def change
+    add_column :likes, :user_id, :int
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_020015) do
+ActiveRecord::Schema.define(version: 2018_10_24_020334) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2018_10_24_020015) do
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
