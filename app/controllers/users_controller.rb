55273de190cd34caaa22cf843efe6bbd0b4aa2b9
troglobite <brianparry089@gmail.com>
2018-10-23 19:34:07 -0600

    added the users articles to the user show page and installed the haml gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def show 
     @user = User.find(params[:id])
     @comments = Comment.where(:user_id => @user.id)
+    @article = Article.where(:user_id => @user.id)
   end
 
   def create
