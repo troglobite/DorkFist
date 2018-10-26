6f528aaec6eff7339912d8fefc07ea15e2667460
troglobite <brianparry089@gmail.com>
2018-10-25 21:01:04 -0600

    contiued work on the like articles feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
     belongs_to :user
-    has_many :comments, :likes, dependent: :destroy
+    has_many :comments, dependent: :destroy
+    has_many :likes, dependent: :destroy
     validates :title, presence: true,
                     length: { minimum: 5 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  root 'article#index'
+  root 'articles#index'
  
   get 'users/new'
   get 'login', to: 'sessions#new'
   get 'logout', to: 'sessions#destroy'
   get 'users/:id', to: 'users#show'
+  get '/users/:user_id/articles/:article_id/likes/new(.:format)', to: 'likes#new'
 
   resources :sessions, only: [:create]
   resources :users, only: [:new, :create, :show]
