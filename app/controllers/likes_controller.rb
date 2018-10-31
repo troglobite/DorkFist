60f3cb4094e4e615f9331c2f326ddf88224d9001
troglobite <brianparry089@gmail.com>
2018-10-30 19:45:27 -0600

    working on making so that only one like per user per article happens. Added a exists on the likes controller that seraches if a like with that user id and article id exists before saving

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -10,9 +10,25 @@ class LikesController < ApplicationController
         @article = params[:article_id]
         likes = {user_id: @user, article_id: @article}
         @like = Like.new(likes)
-        @like.save!
-        if @like.save 
+        if @like.exits?
             redirect_to article_path(@article)
+        else
+            @like.save!
+            if @like.save 
+                redirect_to article_path(@article)
+            end
+
+        end
+    end
+
+    def self.exists?
+        @user = current_user.id
+        @article = params[:article_id]
+        @like = Like.where(user_id: @user.id, article_id: @article.id)
+        if @like
+            return true
+        else 
+            return false
         end
     end
 
