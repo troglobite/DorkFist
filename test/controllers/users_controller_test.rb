5bd76d62436cf2da8e994f80140868589e9ed496
troglobite <brianparry089@gmail.com>
2018-11-20 21:02:22 -0700

    added a logout button to the article index page

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_new_url
+    get users_new
     assert_response :success
   end
 
   test "should get create" do
-    get users_create_url
+    post '/user'
     assert_response :success
   end
 
