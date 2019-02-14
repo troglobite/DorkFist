9e26af6756839af25955579cf6340fed28dd4dc9
troglobite <brianparry089@gmail.com>
2019-02-13 20:24:56 -0700

    Added a active field to the Users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_023315) do
+ActiveRecord::Schema.define(version: 2019_02_14_031346) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2018_10_24_023315) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "active"
   end
 
 end
