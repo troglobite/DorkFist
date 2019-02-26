d8a2187d92c0fa127d8475ded7bdde44a3d515b7
troglobite <brianparry089@gmail.com>
2019-02-25 20:39:27 -0700

    Worked on the edit srticle service - foudn issue with the form partial

--- app/services/edit_article_service.rb
+++ app/services/edit_article_service.rb
@@ -1,5 +1,10 @@
 class EditArticleService
-    def initialize
-        @article = article_params[:article]
+    def initialize(article_params, current_user)
+        @article = Article.find(params[:id])
+        @user = current_user
+    end
+
+    def call 
+        article = Article.update(article_params)
     end
 end
