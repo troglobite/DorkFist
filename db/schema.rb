adb76e7c6bcad7db047f59110add30d3b7e5b9cd
troglobite <brianparry089@gmail.com>
2019-03-07 07:56:45 -0700

    generate the photo model - used paperclip to generate a migration for the photo table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_031346) do
+ActiveRecord::Schema.define(version: 2019_03_07_144957) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -38,6 +38,12 @@ ActiveRecord::Schema.define(version: 2019_02_14_031346) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "article_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
