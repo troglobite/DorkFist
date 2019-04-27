ed38d042630cd467dc2685c364a062c61ff6fee2
troglobite <brianparry089@gmail.com>
2019-04-26 21:22:54 -0600

    removed old test code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,6 @@
 class CommentsController < ApplicationController
 
     def create
-        # @article = Article.find(params[:article_id])
-        # @comment = @article.comments.create(comment_params.merge(user_id: current_user.id))
-        # redirect_to article_path(@article)
         @comment = ::CommentServices::NewCommentService.new(params, current_user).call
         redirect_to article_path(params[:article_id])
     end
@@ -11,27 +8,16 @@ class CommentsController < ApplicationController
     def edit
         @article = Article.find(params[:article_id])
         @comment = Article.joins(:comments)
-        # @comment = Comment.find_by(id: params[:id], article_id: @article.id)
     end
 
     def update
         @article = Article.find(params[:article_id])
-        # @comment = @article.comments.find(params[:id])
-
-        # if @comment.update(comment_params)
-        #     redirect_to @article
-        # else 
-        #     render 'edit'
-        # end
         @comment = ::CommentServices::UpdateCommentService.new(params).call
         redirect_to article_path(@article)
     end
 
     def destroy
         @article = Article.find(params[:article_id])
-        # @comment = @article.comments.find(params[:id])
-        # @comment.destroy
-        # redirect_to article_path(@article)
         @comment = ::CommentServices::DestroyCommentService.new(params).call
         redirect_to article_path(@article)
     end
