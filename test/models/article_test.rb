0a089ef008ff996c2533b36b5c6139b5fed18298
troglobite <brianparry089@gmail.com>
2019-05-02 20:44:11 -0600

    started on the article model tests

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -3,6 +3,12 @@ require 'test_helper'
 class ArticleTest < ActiveSupport::TestCase
   test "should not save without title" do
     article = Article.new
-    assert_not article.save
+    assert_not article.save, "Saved the article without a title"
   end
+
+  test "Should save with a title" do
+    article = Article.new(title: "This is a test", user_id: "1")
+    assert article.save
+  end
+
 end
